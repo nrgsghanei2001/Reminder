8efaf37502ac7f461a31a92af0e9196c527cc60d
nargesghanei2001 <nargesghanei031@gmail.com>
2021-11-21 00:00:02 +0330

    add model task with 5 fields

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+
+class Task(models.Model):
+    categories = (
+        ("1", "University"),
+        ("2", "Maktab"),
+        ("3", "Usual life"),
+        ("4", "entertainment"),
+        ("5", "others"),
+    )
+
+    title = models.CharField(max_length=100, blank=False)
+    explanation = models.TextField(blank=False)
+    category = models.CharField(max_length=1, choices=categories, default="1")
+    priority = models.IntegerField(blank=False)
+    deadline = models.DateField(blank=False)
+
+    def __str__(self) -> str:
+        return self.title
